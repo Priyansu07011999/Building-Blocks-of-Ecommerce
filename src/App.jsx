a7f377cefe479f94f570c8671011a13b749f180f
Priyansu07011999 <147058839+Priyansu07011999@users.noreply.github.com>
2024-04-29 01:02:44 +0530

    Update App.jsx make the home renders

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import About from './components/About/About';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
 
 function App() {
   const [showCart, setShowCart] = useState(false);
@@ -24,9 +25,10 @@ function App() {
           <Routes>
             <Route path="/about" element={<About />} />
             <Route path="/" element={<AppContent />} />
+            <Route path="/home" element={<Home />} /> {/* Fixed the typo here */}
           </Routes>
           <Footer />
-          {showCart && <Cart closeCart={closeCart}/>}
+          {showCart && <Cart closeCart={closeCart} />}
         </div>
       </CartProvider>
     </Router>
