93902e3e29e04cd788d0719a2d419ea88c3c15ac
Priyansu07011999 <147058839+Priyansu07011999@users.noreply.github.com>
2024-04-28 23:18:29 +0530

    Update Header.jsx use react-router-dom

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,30 @@
 import React from 'react';
 import { Navbar, Nav, Button } from 'react-bootstrap';
 import { useCart } from './CartContext';
+import { NavLink } from 'react-router-dom';
 
 function Header({ toggleCart }) {
-    const { cart } = useCart();
-  
-    return (
-      <div style={{ textAlign: 'center' }}>
-        <Navbar bg="light" expand="lg">
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <div style={{ backgroundColor: 'black', padding: '10px' }}>
-                <Nav.Link style={{ color: 'white', padding: '3rem' }} href="#home">HOME</Nav.Link>
-                <Nav.Link style={{ color: 'white', padding: '3rem' }} href="#store">STORE</Nav.Link>
-                <Nav.Link style={{ color: 'white', padding: '3rem' }} href="#about">ABOUT</Nav.Link>
-                <Button style={{ color: 'white', marginLeft: '30px' }} onClick={toggleCart}>CART ({cart.length})</Button>
-              </div>
-            </Nav>
-          </Navbar.Collapse>
-          <div style={{ backgroundColor: 'gray', padding: '30px' }}>
-            <Navbar.Brand href="#home" style={{ color: 'white', fontSize: '72px' }}>The Generics</Navbar.Brand>
-          </div>
-        </Navbar>
-      </div>
-    );
-  }
+  const { cart } = useCart();
 
-export default Header
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <div style={{ backgroundColor: 'black', padding: '10px' }}>
+              <NavLink to="/" style={{ color: 'white', padding: '3rem' }}>HOME</NavLink>
+              <NavLink to="/content" style={{ color: 'white', padding: '3rem' }}>STORE</NavLink>
+              <NavLink to="/about" style={{ color: 'white', padding: '3rem' }}>ABOUT</NavLink>
+              <Button style={{ color: 'white', marginLeft: '30px' }} onClick={toggleCart}>CART ({cart.length})</Button>
+            </div>
+          </Nav>
+        </Navbar.Collapse>
+        <div style={{ backgroundColor: 'gray', padding: '30px' }}>
+          <Navbar.Brand href="#home" style={{ color: 'white', fontSize: '72px' }}>The Generics</Navbar.Brand>
+        </div>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Header;
