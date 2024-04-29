cef8e8ac1a37a3bda1ecdb9fc362ce6163958e1d
Priyansu07011999 <147058839+Priyansu07011999@users.noreply.github.com>
2024-04-30 01:21:42 +0530

    Update App.jsx Adding Contact US Page via REACT ROUTER

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Cart from './components/Cart';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import About from './components/About/About';
+import ContactMe from './components/Contact/ContactMe';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 
@@ -23,9 +24,10 @@ function App() {
         <div>
           <Header toggleCart={toggleCart} cartLength={Cart.length} />
           <Routes>
-            <Route path="/about" element={<About />} />
             <Route path="/" element={<AppContent />} />
-            <Route path="/home" element={<Home />} /> {/* Fixed the typo here */}
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<ContactMe />} />
           </Routes>
           <Footer />
           {showCart && <Cart closeCart={closeCart} />}
