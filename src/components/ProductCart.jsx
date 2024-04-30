78cbf33333c379078c272bf47e898a8b65cd6d2e
Priyansu07011999 <147058839+Priyansu07011999@users.noreply.github.com>
2024-04-30 11:05:51 +0530

    Update ProductCart.jsx Implement the Products Page

--- src/components/ProductCart.jsx
+++ src/components/ProductCart.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import { Card, Button} from 'react-bootstrap';
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function ProductCard({ product, addToCart }) {
-    const handleAddToCart = () => {
-      addToCart(product);
-    };
-  
-    return (
-      <Card style={{ width: '18rem' }}>
-        <Card.Text style={{text: 'bold', textAlign: 'center', marginTop: '1rem',marginBottom: '10px'  ,fontFamily: 'Arial, sans-serif', fontWeight: 'bold'}}> {product.title}</Card.Text>
+  const handleAddToCart = () => {
+    addToCart(product);
+  };
+
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Text style={{text: 'bold', textAlign: 'center', marginTop: '1rem',marginBottom: '10px'  ,fontFamily: 'Arial, sans-serif', fontWeight: 'bold'}}> {product.title}</Card.Text>
+      <Link to={`/product/${product.id}`}>
         <Card.Img variant="top" src={product.imageUrl} />
-        <Card.Body style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <div>
-            <Card.Text>${product.price}</Card.Text>
-          </div>
-          <Button onClick={handleAddToCart} style={{ border: '1px solid blue', backgroundColor: 'blue', color: 'white', borderRadius: '0px', padding: '0px 10px', marginTop: '10px' }} variant="primary">Add to Cart</Button>
+      </Link>
+      <Card.Body style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <div>
+          <Card.Text>${product.price}</Card.Text>
+         </div>
+         <Button onClick={handleAddToCart} style={{ border: '1px solid blue', backgroundColor: 'blue', color: 'white', borderRadius: '0px', padding: '0px 10px', marginTop: '10px' }} variant="primary">Add to Cart</Button>
         </Card.Body>
       </Card>
-    );
-  }
+  );
+}
 
-export default ProductCard
+export default ProductCard;
+
